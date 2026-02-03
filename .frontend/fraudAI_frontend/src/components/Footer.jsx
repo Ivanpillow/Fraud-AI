5b833f28ca086d18c4d23f4946461bc277515818
Angel De La Cruz <luigi9221.ladlca@gmail.com>
2026-02-03 10:56:08 -0600

    Remove nested frontend app and add deps
    Delete the .frontend/fraudAI_frontend React/Vite app and its related source/config files to consolidate frontend code. Add frontend dependency files (package lock and requirements) at the frontend/ root. Also include a number of updated Python .pyc files (compiled bytecode) from the backend.

--- .frontend/fraudAI_frontend/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 py-6 text-center text-gray-400">
-      © {new Date().getFullYear()} FraudAI - Todos los derechos reservados
-    </footer>
-  );
-}
